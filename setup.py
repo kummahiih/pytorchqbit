d21137015e16d65d5259a9ae5b17d88c5c6d5b09
kummap <pauli.rikula@gmail.com>
2023-03-16 22:26:12 +0200

    verion

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.md', 'r') as readme_file:
 
 setup(
     name='pytorchqbit',
-    version='0.2.3',
+    version='0.0.1',
     description='Quantum bit and the usual gates in torch tensors straight from the Wikipedia',
     long_description=README,
     license="MIT",
